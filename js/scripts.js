0344dfdd35389ea8eff324e7a6bd86e91fc7f051
Tom Chege <emdeechege@gmail.com>
2018-07-13 15:55:12 +0300

    styling column two

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // alert("tom");
 $(document).ready(function() {
   $("form#ping").submit(function(event) {
-      event.preventDefault();
-      var userInput = parseInt($("input#pingNum").val());
+    event.preventDefault();
+    var userInput = parseInt($("input#pingNum").val());
 
-      for (var i = 1; i <= userInput; i++) {
-        var result = "";
+    for (var i = 1; i <= userInput; i++) {
+      var result = "";
       if (i % 3 != 0 && i % 5 != 0) result = (i)
       if (i % 3 === 0) result = "ping"
       if (i % 5 === 0) result += "pong"
