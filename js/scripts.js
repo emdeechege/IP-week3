9fab3c2deabdae4c9a66cde83bde5d288a751f07
Tom Chege <emdeechege@gmail.com>
2018-07-13 12:30:08 +0300

    Value now being returned

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
-$(document).ready(function(){
-  $(form#ping).submit()
-event.preventDefault
-
+// alert("tom");
+$(document).ready(function() {
+  $("form#ping").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("input#pingNum").val();
+    var result = checkOut(userInput);
+  $("#display").text(result);
+    // console.log(userInput);
+    // $("p#display").text(userInput);
+  })
+})
+var checkOut=function(userInput) {
+for (var i = 1; i <= userInput; i++)
+  if (userInput % 3 === 0 && userInput % 5 === 0) {
+    return ("pingpong");
+  } else if (userInput % 5 === 0) {
+  return ("ping");
+} else if (userInput % 3 === 0) {
+  return ("pong");
+} else {
+  return(userInput);
+}
 }
