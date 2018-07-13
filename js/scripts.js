61974eda68a831538e389dbed73a4b7eba8006d8
Tom Chege <emdeechege@gmail.com>
2018-07-13 17:41:41 +0300

    refreshes input contents to display new results

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
-// alert("tom");
+//user interface logic
 $(document).ready(function() {
   $("form#ping").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("input#pingNum").val());
 
+//refreshes the filled in data to return new input values
+$("#display").empty("");
+
+//business logic
     for (var i = 1; i <= userInput; i++) {
       var result = "";
       if (i % 3 != 0 && i % 5 != 0) result = (i)
